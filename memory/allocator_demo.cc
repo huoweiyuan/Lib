ce5d4c7ec00dfda65a24cfdfab213256e0084450
huoweiyuan <huoweiyuan@foxmail.com>
2022-06-03 12:34:54 +0800

    remove delete allocator ptr

---
+++ memory/allocator_demo.cc
@@ -0,0 +1,151 @@
+#include "zy_allocator.h"
+#include <iostream>
+#include <cstdlib>
+#include <cstdlib>
+#include <list>
+using namespace std;
+using namespace zy;
+
+class TestAllocator : public Allocator
+{
+ private:
+  const int ALLOC_HEAD_SIZE = sizeof(size_t);
+  const int READ_ALLOC_SIZE_OFFSET = -(ALLOC_HEAD_SIZE);
+
+ public:
+  char* alloc(std::size_t size)
+  {
+    size_t _alloc_size = size + ALLOC_HEAD_SIZE;
+    void *ptr = ::malloc(_alloc_size);
+    *static_cast<size_t*>(ptr) = _alloc_size;
+    return static_cast<char*>(ptr) + ALLOC_HEAD_SIZE;
+  }
+
+  char* realloc(void *ptr, size_t size)
+  {
+    if (ptr != nullptr)
+    {
+      ptr = static_cast<char*>(ptr) + READ_ALLOC_SIZE_OFFSET;
+    }
+
+    size_t _realloc_size = size + ALLOC_HEAD_SIZE;
+    ptr = ::realloc(ptr, _realloc_size);
+    *static_cast<size_t*>(ptr) = _realloc_size;
+    return static_cast<char*>(ptr) + ALLOC_HEAD_SIZE;
+  }
+
+  void free(void *ptr)
+  {
+    ::free(static_cast<char*>(ptr) + READ_ALLOC_SIZE_OFFSET);
+  }
+
+};
+
+TestAllocator *g_allocator = new TestAllocator();
+
+
+class Test_ref
+{
+ public:
+  std::list<char> &m_lst_ref;
+  Test_ref(std::list<char> &lst_ref)
+    : m_lst_ref(lst_ref)
+  {
+   }
+};
+
+class Test
+{
+ private:
+  std::list<char> m_lst;
+  std::list<char> *m_lst_ptr;
+  std::list<char> &m_lst_ref;
+  std::list<char> *m_lst_ptr_alloc;
+  Test_ref *m_test_ref_ptr;
+ public:
+  Test()
+    : m_lst_ptr(&m_lst),
+      m_lst_ref(m_lst),
+      m_lst_ptr_alloc(g_new<std::list<char> >(g_allocator)),
+      m_test_ref_ptr(g_new<Test_ref>(g_allocator, m_lst))
+  {
+
+    cout
+      << "Test::Test, "
+      << (void*)(&m_lst)
+      << " "
+      << (void*)(m_lst_ptr)
+      << " "
+      << (void*)(&m_lst_ref)
+      << " "
+      << (void*)(&(m_test_ref_ptr->m_lst_ref))
+      << endl;
+  }
+
+  Test(int a)
+    : m_lst_ptr(&m_lst),
+      m_lst_ref(m_lst)
+
+  {
+    cout << "Test::Test(int)"
+         << (void*)(&m_lst)
+         << " "
+         << (void*)(m_lst_ptr)
+         << (void*)(&m_lst_ref)
+         << endl;
+  }
+
+  Test(int a, int b)
+    : m_lst_ptr(&m_lst),
+      m_lst_ref(m_lst)
+  {
+    cout << "Test::Test(int,int)"
+         << (void*)(&m_lst)
+         << " "
+         << (void*)(m_lst_ptr)
+         << (void*)(&m_lst_ref)
+         << endl;
+  }
+
+  Test(Allocator *a, int b)
+    : m_lst_ptr(&m_lst),
+      m_lst_ref(m_lst)
+  {
+    cout << "Test::Test(Allocator*,int)"
+         << (void*)(&m_lst)
+         << " "
+         << (void*)(m_lst_ptr)
+         << (void*)(&m_lst_ref)
+         << endl;
+  }
+
+  ~Test()
+  {
+    cout << "Test::~Test()" << endl;
+  }
+};
+
+
+template <typename T>
+class Ref_test
+{
+ public:
+  T &m_ref;
+  Ref_test(T &ref)
+    :m_ref(ref)
+  {
+    cout << "m_ref " << (void*)(&m_ref) << " value " << m_ref << endl;
+  }
+};
+
+int main()
+{
+  Test *t2 = g_new<Test, 10>(g_allocator);
+  g_delete(t2);
+
+  int ref = 1;
+  cout << "ref " << (void*)(&ref) << endl;
+  Ref_test<int> *_ref = g_new<Ref_test<int>, 3>(g_allocator, ref);
+  g_delete(_ref);
+  return 0;
+}
---
+++ memory/zy_allocator.h
@@ -0,0 +1,150 @@
+#ifndef _ZY_ALLOCATOR_
+#define _ZY_ALLOCATOR_
+
+#include <cstddef>
+#include <utility>
+
+namespace zy
+{
+class Allocator
+{
+ private:
+   static const short OBJ_NUM_HEAD_SIZE = sizeof(size_t);
+   static const short OBJ_PTR_HEAD_SIZE = sizeof(void*);
+   static const short READ_OBJ_NUM_OFFSET = -(OBJ_NUM_HEAD_SIZE) -(OBJ_PTR_HEAD_SIZE);
+   static const short READ_OBJ_PTR_OFFSET = -(OBJ_PTR_HEAD_SIZE);
+
+ public:
+   virtual char* alloc(std::size_t size) = 0;
+   virtual char* realloc(void *ptr, size_t size) = 0;
+   virtual void free(void *ptr) = 0;
+
+ public:
+   // |obj num|obj pointer|xxxx
+   template<typename ClassName, size_t Num, typename... P> 
+     ClassName* create_obj(P&&... args)
+     {
+       const size_t _class_size = sizeof(ClassName);
+       char *_ptr = 
+         alloc(_class_size * Num + OBJ_NUM_HEAD_SIZE + OBJ_PTR_HEAD_SIZE);
+       if (_ptr == nullptr)
+         return nullptr;
+
+       *(reinterpret_cast<size_t*>(_ptr)) = Num;
+       _ptr += OBJ_NUM_HEAD_SIZE;
+       *(reinterpret_cast<Allocator**>(_ptr)) = this;
+       _ptr += OBJ_PTR_HEAD_SIZE;
+
+       ClassName *_construct_ptr = reinterpret_cast<ClassName*>(_ptr);
+       for (size_t i = 0; i < Num; i++)
+       {
+         new(_construct_ptr + i) ClassName(std::forward<P>(args)...);
+       }
+       return _construct_ptr;
+     }
+
+   template<typename ClassName>
+     void delete_obj(ClassName *_destruct_ptr)
+     {
+       if (_destruct_ptr == nullptr)
+         return;
+       char *_alloced_ptr = 
+         reinterpret_cast<char*>(_destruct_ptr) + READ_OBJ_NUM_OFFSET;
+       const size_t _obj_num = *(reinterpret_cast<size_t*>(_alloced_ptr));
+       for (size_t i = 0; i < _obj_num; i++)
+       {
+         (_destruct_ptr + i)->~ClassName();
+       }
+       free(_alloced_ptr);
+     }
+
+ public:
+   // |obj pointer|xxxx
+   char* alloc_memory(size_t size) {
+     char* _ptr = alloc(size + OBJ_PTR_HEAD_SIZE);
+     if (_ptr == nullptr) 
+       return nullptr;
+     *(reinterpret_cast<Allocator**>(_ptr)) = this;
+     return _ptr + OBJ_PTR_HEAD_SIZE;
+   }
+
+   char* realloc_memory(void *ptr, size_t size) {
+     if (ptr == nullptr) 
+       return nullptr;
+     char *_ptr = realloc(static_cast<char*>(ptr) + READ_OBJ_PTR_OFFSET, size + OBJ_PTR_HEAD_SIZE);
+     if (_ptr == nullptr)
+       return nullptr;
+     return _ptr + OBJ_PTR_HEAD_SIZE;
+   }
+
+   void free_memory(void *ptr) {
+     if (ptr == nullptr)
+       return;
+     free(static_cast<char*>(ptr) + READ_OBJ_PTR_OFFSET);
+   }
+
+ public:
+   Allocator() noexcept = default;
+   virtual ~Allocator() noexcept = default;
+
+ public:
+   static Allocator* get_allocator_ptr(void *ptr) {
+     if (ptr == nullptr)
+       return nullptr;
+     return *(reinterpret_cast<Allocator**>(
+       static_cast<char*>(ptr) + READ_OBJ_PTR_OFFSET));
+   }
+};
+
+}
+
+namespace zy
+{
+
+inline char* g_alloc(Allocator *allocator, size_t size)
+{
+  if (allocator == nullptr)
+    return nullptr;
+  return allocator->alloc(size);
+}
+
+inline char* g_realloc(void *ptr, size_t size)
+{
+  Allocator *allocator = nullptr; 
+  if (ptr == nullptr || 
+    (allocator = Allocator::get_allocator_ptr(ptr)) == nullptr)
+    return nullptr;
+
+  return allocator->realloc(ptr, size);
+}
+
+inline void g_free(void *ptr)
+{
+  Allocator *allocator = nullptr; 
+  if (ptr == nullptr ||
+    (allocator = Allocator::get_allocator_ptr(ptr)) == nullptr)
+    return;
+  allocator->free(ptr);
+}
+
+  template<typename ClassName, size_t Num = 1, typename... P>
+ClassName* g_new(Allocator *allocator, P&&... args)
+{
+  if (allocator == nullptr)
+    return nullptr;
+  ClassName *_ptr = allocator->create_obj<ClassName, Num>(std::forward<P>(args)...);
+  return _ptr;
+}
+
+  template<typename ClassName>
+void g_delete(ClassName *ptr)
+{
+  Allocator *allocator = nullptr;
+  if (ptr == nullptr || 
+    (allocator = Allocator::get_allocator_ptr(ptr)) == nullptr)
+    return;
+  allocator->delete_obj(ptr);
+}
+}
+
+#endif // _ZY_ALLOCATOR_
